a869115efb38c2786d207e13de5b596098a83e83
Fadel <fadel.g.heji@gmail.com>
2022-07-30 08:47:36 -0400

    commit tree 1fc1485b5b4a4a2704b0f9256f060ceabfbf70ac

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require "pry"
+class Anagram
+    attr_accessor :word, :word_array
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match word_array
+        word_array.select do |words|
+            words.chars.sort == word.chars.sort
+        end
+    end
+end
+
